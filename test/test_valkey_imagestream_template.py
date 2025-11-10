19425b9902f778cc9a8217efdfe469b8889ce5b1
Petr "Stone" Hracek <phracek@redhat.com>
2025-11-10 15:02:36 +0100

    Skip testing RHEL9 for OpenShift 4. It is not supported yet.
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

--- test/test_valkey_imagestream_template.py
+++ test/test_valkey_imagestream_template.py
@@ -2,30 +2,28 @@ import pytest
 
 from container_ci_suite.openshift import OpenShiftAPI
 
-from conftest import VARS, skip_valkey_for_rhel9
+from conftest import VARS, skip_for_rhel9
 
 
 class TestValkeyImagestreamTemplate:
-
     def setup_method(self):
-        self.oc_api = OpenShiftAPI(pod_name_prefix="valkey", version=VARS.VERSION, shared_cluster=True)
+        self.oc_api = OpenShiftAPI(
+            pod_name_prefix="valkey", version=VARS.VERSION, shared_cluster=True
+        )
 
     def teardown_method(self):
         self.oc_api.delete_project()
 
     @pytest.mark.parametrize(
         "template",
-        [
-            "valkey-ephemeral-template.json",
-            "valkey-persistent-template.json"
-        ]
+        ["valkey-ephemeral-template.json", "valkey-persistent-template.json"],
     )
     def test_valkey_imagestream_template(self, template):
-        skip_valkey_for_rhel9()
-        os_name = ''.join(i for i in VARS.OS if not i.isdigit())
+        skip_for_rhel9()
+        os_name = "".join(i for i in VARS.OS if not i.isdigit())
         assert self.oc_api.deploy_image_stream_template(
             imagestream_file=f"imagestreams/valkey-{os_name}.json",
             template_file=f"examples/{template}",
-            app_name=self.oc_api.pod_name_prefix
+            app_name=self.oc_api.pod_name_prefix,
         )
         assert self.oc_api.is_pod_running(pod_name_prefix=self.oc_api.pod_name_prefix)
--- test/test_valkey_template.py
+++ test/test_valkey_template.py
@@ -2,26 +2,24 @@ import pytest
 
 from container_ci_suite.openshift import OpenShiftAPI
 
-from conftest import VARS, skip_valkey_for_rhel9
+from conftest import VARS, skip_for_rhel9
 
 
 class TestRedisDeployTemplate:
-
     def setup_method(self):
-        self.oc_api = OpenShiftAPI(pod_name_prefix="valkey", version=VARS.VERSION, shared_cluster=True)
+        self.oc_api = OpenShiftAPI(
+            pod_name_prefix="valkey", version=VARS.VERSION, shared_cluster=True
+        )
 
     def teardown_method(self):
         self.oc_api.delete_project()
 
     @pytest.mark.parametrize(
         "template",
-        [
-            "valkey-ephemeral-template.json",
-            "valkey-persistent-template.json"
-        ]
+        ["valkey-ephemeral-template.json", "valkey-persistent-template.json"],
     )
     def test_valkey_template_inside_cluster(self, template):
-        skip_valkey_for_rhel9()
+        skip_for_rhel9()
         assert self.oc_api.deploy_template_with_image(
             image_name=VARS.IMAGE_NAME,
             template=f"examples/{template}",
@@ -29,8 +27,8 @@ class TestRedisDeployTemplate:
             openshift_args=[
                 f"VALKEY_VERSION={VARS.VERSION}",
                 f"DATABASE_SERVICE_NAME={self.oc_api.pod_name_prefix}",
-                "VALKEY_PASSWORD=testp"
-            ]
+                "VALKEY_PASSWORD=testp",
+            ],
         )
 
         assert self.oc_api.is_pod_running(pod_name_prefix=self.oc_api.pod_name_prefix)
@@ -38,5 +36,5 @@ class TestRedisDeployTemplate:
             image_name=f"registry.redhat.io/{VARS.OS}/valkey-{VARS.VERSION}",
             service_name=self.oc_api.pod_name_prefix,
             cmd="timeout 15 valkey-cli -h <IP> -a testp ping",
-            expected_output="PONG"
+            expected_output="PONG",
         )
